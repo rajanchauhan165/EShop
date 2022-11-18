f4d186a70bd5997d169106f490d23fe88228b472
Rajan Chauhan <95286494+rajanchauhan165@users.noreply.github.com>
2022-11-18 10:11:41 +0530

    Add files via upload

---
+++ src/test/java/com/rajan/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
